ecdf624af716b26fe0e86be0bebbb549784421b9
Abhisek Sanyal <10907245+abhiseksanyal@users.noreply.github.com>
2024-10-02 16:05:21 +0530

    Skip missing files during walk in Binary artifact check (#10)
    Continue with file walk of repository during Binary artifact check instead of failing

--- clients/localdir/client.go
+++ clients/localdir/client.go
@@ -96,6 +96,12 @@ func listFiles(clientPath string) ([]string, error) {
 			return fmt.Errorf("failure accessing path %q: %w", pathfn, err)
 		}
 
+		// Ignore files that does not exist. e.g - If it is a symlink and the target does not exist
+		_, err = os.Stat(pathfn)
+		if os.IsNotExist(err) {
+			return nil
+		}
+
 		// Skip directories.
 		d, err := isDir(pathfn)
 		if err != nil {
